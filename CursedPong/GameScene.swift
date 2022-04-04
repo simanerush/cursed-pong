30625d767588b089bcfa985703bc9262125d90a1
Sima Nerush <2002ssn@gmail.com>
2022-04-04 13:34:57 -0700

    Harrison's changes

--- CursedPong/GameScene.swift
+++ CursedPong/GameScene.swift
@@ -10,8 +10,8 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-//    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //    private var label : SKLabelNode?
+    //    private var spinnyNode : SKShapeNode?
     private var player: SKShapeNode?
     private var aiPlayer: SKShapeNode?
     private var ball: SKShapeNode?
@@ -21,24 +21,24 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
-//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-//        if let label = self.label {
-//            label.alpha = 0.0
-//            label.run(SKAction.fadeIn(withDuration: 2.0))
-//        }
+        //        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        //        if let label = self.label {
+        //            label.alpha = 0.0
+        //            label.run(SKAction.fadeIn(withDuration: 2.0))
+        //        }
         
         // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //        let w = (self.size.width + self.size.height) * 0.05
+        //        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        //        if let spinnyNode = self.spinnyNode {
+        //            spinnyNode.lineWidth = 2.5
+        //
+        //            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+        //            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+        //                                              SKAction.fadeOut(withDuration: 0.5),
+        //                                              SKAction.removeFromParent()]))
+        //        }
         
         self.player = SKShapeNode.init(rectOf: CGSize(width: 250, height: 50))
         if let rect = self.player {
@@ -64,7 +64,7 @@ class GameScene: SKScene {
         self.ball = SKShapeNode.init(circleOfRadius: 30)
         if let ball = self.ball {
             let ballPhysics = SKPhysicsBody()
-            ballPhysics.velocity = CGVector(dx: Int.random(in: 100...500), dy: Int.random(in: 100...500))
+            ballPhysics.velocity = CGVector(dx: Int.random(in: 100...500), dy: Int.random(in: 500...1000))
             if ballPhysics.velocity.dy > 0 {
                 self.ballIsGoingUp = true
             } else {
@@ -88,19 +88,19 @@ class GameScene: SKScene {
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        //        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        //            n.position = pos
+        //            n.strokeColor = SKColor.green
+        //            self.addChild(n)
+        //        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        //        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        //            n.position = pos
+        //            n.strokeColor = SKColor.blue
+        //            self.addChild(n)
+        //        }
         
         if let rect = self.player {
             rect.position = CGPoint(x: pos.x, y: self.frame.minY + rect.frame.height * 1.5)
@@ -108,17 +108,17 @@ class GameScene: SKScene {
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        //        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        //            n.position = pos
+        //            n.strokeColor = SKColor.red
+        //            self.addChild(n)
+        //        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        if let label = self.label {
-//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-//        }
+        //        if let label = self.label {
+        //            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        //        }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
@@ -138,27 +138,32 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         if let ball = self.ball {
-        
-            if ball.position.x > self.frame.maxX - ball.frame.size.width || ball.position.x < self.frame.minX + ball.frame.size.width{
+            
+            if ball.position.x > self.frame.maxX - ball.frame.size.width * 1.5 || ball.position.x < self.frame.minX + ball.frame.size.width * 1.5 {
                 ball.physicsBody!.velocity.dx *= -1
             }
+            
             if ball.position.y > self.frame.maxY - ball.frame.size.height{
                 ball.physicsBody!.velocity.dy *= -1
             }
+            
             if ball.position.y < self.player!.position.y + ball.frame.height && ball.position.x > self.player!.position.x - self.player!.frame.width && ball.position.x < self.player!.position.x + self.player!.frame.width && !self.ballIsGoingUp! {
                 ball.physicsBody!.velocity.dy *= -1
-                ball.physicsBody!.velocity.dx = abs(ball.position.x - player!.position.x)/1
+                ball.physicsBody!.velocity.dx = (ball.position.x - player!.position.x)/1
                 self.ballIsGoingUp = true
             }
+            
             if ball.position.y > self.aiPlayer!.position.y - ball.frame.height && ball.position.x > self.aiPlayer!.position.x - self.aiPlayer!.frame.width && ball.position.x < self.aiPlayer!.position.x + self.aiPlayer!.frame.width  && self.ballIsGoingUp! {
                 ball.physicsBody!.velocity.dy *= -1
-                ball.physicsBody!.velocity.dx = abs(ball.position.x - aiPlayer!.position.x)/1
+                ball.physicsBody!.velocity.dx = (ball.position.x - aiPlayer!.position.x)/1
                 self.ballIsGoingUp = false
             }
         }
         if let player2 = self.aiPlayer {
-            player2.physicsBody!.velocity.dx = -player2.position.x + self.ball!.position.x
+            let distanceToBall = self.ball!.position.x - player2.position.x
+            player2.physicsBody!.velocity.dx = distanceToBall * 3
         }
         // Called before each frame is rendered
     }
 }
+
