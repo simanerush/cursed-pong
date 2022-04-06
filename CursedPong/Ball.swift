48e94d427dbca607236393dbbcdcf431dff1c3e7
Harrison Nicholls <nichollsharrisonj@gmail.com>
2022-04-05 17:08:07 -0700

    added ball and player class

---
+++ CursedPong/Ball.swift
@@ -0,0 +1,8 @@
+//
+//  Ball.swift
+//  CursedPong
+//
+//  Created by Harrison N on 4/5/22.
+//
+
+import Foundation
---
+++ CursedPong/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  CursedPong
+//
+//  Created by Harrison N on 4/5/22.
+//
+
+import Foundation
+import SpriteKit
+
+class Player {
+    private var shapeNode: SKShapeNode
+    
+    init(shapeNode: SKShapeNode) {
+        self.shapeNode = shapeNode
+    }
+    
+}
